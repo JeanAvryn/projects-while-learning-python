57e4da1c1834fdbbaa32dd6e47fa61a3b7ded903
Abrin <145266464+JeanAvryn@users.noreply.github.com>
2025-12-26 22:27:19 +0800

    Create findtheitem.py

---
+++ problemSolving/findtheitem.py
@@ -0,0 +1,10 @@
+titles = [
+    "How to learn Python", 
+    "Java for beginners", 
+    "Python data science", 
+    "The 10 best coding tips"
+]
+keyword = "Python"
+print([index for index, title in enumerate(titles) if keyword in title])
+
+        
